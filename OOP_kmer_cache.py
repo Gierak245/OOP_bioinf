2ce6e3f158737d55fc616efd09cac8f039533796
Jakub Giera <j.giera01@gmail.com>
2025-07-04 06:41:59 +0200

    adding nessesary files

---
+++ OOP_kmer_cache.py
@@ -0,0 +1,250 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 30 19:22:59 2025
+
+@author: giera
+"""
+
+from hashlib import sha256
+import os
+import pickle
+from functools import wraps
+import re
+
+
+
+def disk_cache(cache_dir="cache"):
+    
+    if os.path.exists(cache_dir) != True:
+        os.makedirs(cache_dir)
+        
+            
+    def decorator(func):
+        
+        @wraps(func)
+        def wrapper(arg):
+            # Checking if given arg (Class) has a sequence string
+            if hasattr(arg, 'sequence_string'):
+                
+                # hetting sequence from Class
+                seq_text = arg.sequence_string
+                key = sha256(seq_text.encode('utf‑8')).hexdigest()
+            
+            # path to pickle file in cache folder
+            key_path = os.path.normpath(os.path.join(cache_dir, f'{key}.pkl'))
+            
+            # Loading or creating and than loading a pkl file.
+            if os.path.exists(key_path) == True:
+                print(f"Loading from cache data for {arg.header}")
+                with open(key_path, 'rb') as pickle_file:  
+                    kmer_db = pickle.load(pickle_file)
+                
+                return kmer_db
+            
+            else:
+                # return function output
+                result = func(arg)
+                
+                # writing result into a file
+                with open(key_path, 'wb') as pickle_file:
+                    pickle.dump(result, pickle_file)
+                
+                return result
+        
+        return wrapper
+    return decorator
+
+
+
+class Sequence:
+    
+    def __init__(self, header, sequence_string, quality):
+        self.header = header
+        self.sequence_string = sequence_string
+        self.quality = quality
+    
+    def __repr__(self):
+        return f'''Sequence header: {self.header}
+Sequence: {self.sequence_string}
+Length: {len(self.sequence_string)}
+'''
+    
+    # Classmethod creating a Sequence Class from fasta file
+    @classmethod
+    def from_fasta(cls, text: str):
+        
+        # creating temp headers and seq
+        header = ''
+        sequence_string = ''
+        
+        # loop to get header and sequence from file
+        
+        for line in text.strip().splitlines():
+            if line.startswith(">"):
+                header = line.strip()[1:]
+            else:
+               sequence_string += line.strip()
+               
+        if sequence_string == '':
+            raise ValueError("Sequence is empty. Please check your FASTA file.")
+        
+        return cls(header, sequence_string, quality = None)
+    
+    # Classmethod creating a Sequence Class from fastq file
+    @classmethod
+    def from_fastq(cls, text: str):
+        
+        # creating headers and sequence_string
+        
+        splitted_text = text.strip().splitlines()
+        
+        if len(splitted_text) < 4:
+            raise ValueError("Your Fastq file is too short (less than 4 lines). Please check your file")
+        
+        header = splitted_text[0][1:]
+        sequence_string = splitted_text[1]
+        quality_string = splitted_text[3]
+        
+        if quality_string == '':
+            raise ValueError("Quality string is empty. Please check your FASTQ file.")
+        
+
+        return cls(header, sequence_string, quality_string)
+    
+    @classmethod
+    def parse_fasta_file(cls, filepath):
+        
+        with open(filepath, 'r') as f:
+            
+            header, seq_lines = None, []
+            
+            for line in f.readlines():
+                line = line.rstrip()
+                if line.startswith('>'):
+                    if header:
+                        yield cls(header, ''.join(seq_lines), None)                
+                    header = line[1:]
+                    
+                    seq_lines = []
+                    
+                else:
+                    seq_lines.append(line)
+            if header and seq_lines:
+                yield cls(header, ''.join(seq_lines), None)   
+            
+    @classmethod
+    def parse_fastq_file(cls, filepath):
+        
+        with open(filepath, 'r') as f:
+            
+            file = f.readlines()
+            header, sequence_string, quality = None, None, None
+            
+            i = 0
+            
+            while i <= len(file) - 4:
+                
+                header = file[i]
+                sequence_string = file[i + 1]
+                quality = file[i + 3]
+                
+                yield cls(header, sequence_string, quality)
+                i += 4
+            
+                    
+   # @disk_cache()
+    def expensive_kmer(self):
+        
+        k = len(self.sequence_string)
+        k_mers = {}
+
+        for i in range(1, k + 1):
+            temp_list = []
+            for j in range(0, k + 1):
+                if j + i <= k:
+                    temp_list.append(self.sequence_string[j : j + i])
+                k_mers[i] = temp_list
+        
+        return k_mers
+        
+
+seq = Sequence.from_fasta(">s1\nATGC\n")
+# First call: should compute and create "cache/<hash>.pkl"
+db1 = seq.expensive_kmer()
+# Second call: should load from disk, not recompute
+db2 = seq.expensive_kmer()
+
+fasta = """>seq1
+        AGCTTGA
+        GCTTATT
+        CCCAAGT"""
+
+fastq = """@seq2
+GGTTAACC
++
+IIIIIIII
+"""
+
+# for line in text.strip().splitlines(): print(line.startswith(">"))
+
+test_fasta = Sequence.from_fasta(fasta)
+test_fasta
+
+test_fastq = Sequence.from_fastq(fastq)
+test_fastq
+
+
+cache_patch = "C:/Users/giera/OneDrive/Dokumenty/Python_Scripts/cache"
+
+# creating key and assiging sequence 
+key = sha256(test_fasta.sequence_string.encode()).hexdigest()
+db = {}
+db[key] = test_fasta.sequence_string
+
+# Checking if catche path is present 
+os.path.exists(cache_patch)
+
+fasta_file = os.path.normpath(r"C:\Users\giera\OneDrive\Dokumenty\Python_Scripts\OOP\ex_fasta.txt")
+
+for seq in Sequence.parse_fasta_file(fasta_file):
+    kmer_dict = seq.expensive_kmer()
+
+# path_to_pckle = os.path.normpath(os.path.join(cache_patch, f'{key}.pkl'))
+# print(path_to_pckle)
+# print(os.path.exists(path_to_pckle))
+
+
+# with open('test.pkl', 'rb') as f:
+#     db1 = pickle.load(f)
+#     print(db1)
+
+# seq = "ATTGCC"
+
+# def expensive(seq):
+    
+#     k = len(seq)
+#     k_mers = {}
+
+#     for i in range(1, k + 1):
+#         temp_list = []
+#         for j in range(0, k + 1):
+#             if j + i <= k:
+#                 temp_list.append(seq[j : j + i])
+#             k_mers[i] = temp_list
+    
+#     return k_mers
+
+# k_mers = expensive(test_fasta.sequence_string)
+
+# sequence_key = sha256(test_fasta.sequence_string.encode()).hexdigest()
+
+# with open(f'{sequence_key}.pkl', 'wb') as pickle_file:  
+#     pickle_test = pickle.dump(sequence_key, pickle_file)
+# pickle_file.close()
+
+# for file in os.listdir('cache'):
+#     print(re.match(".*pkl", file).string)
+#     to_remove = re.match(".*pkl", file).string
+#     os.remove(f'cache/{to_remove}')
+
+# os.listdir('cache')
---
+++ seqtools.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  3 09:57:22 2025
+
+@author: giera
+"""
+from  OOP_learning import Sequence, disk_cache
+import argparse
+import os
+import pickle
+import re
+import zlib
+import zipfile
+from datetime import datetime, date
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", required=True, help="Input file path")
+parser.add_argument("-o","--output", required = True, help="Output path for your pickle file")
+parser.add_argument("--clear_cache", default=False ,choices=[True, False] ,help="Clears out cache folder. True = Yes, False = No")
+
+args = parser.parse_args()
+
+fasta_formats = ('fasta', 'fa', 'fna', 'ffn', 'faa', 'frn', 'mpfa')
+
+file = 'test.fa'
+
+zip_path = os.path.join(args.output, 'old_cache.zip')
+
+def main():
+    
+    # checks if file is fasta or fastq format
+    if str(args.input).endswith('.fastq'):
+        records = Sequence.parse_fastq_file(args.input)
+    elif args.input.split('.')[-1] in fasta_formats:
+        records = Sequence.parse_fasta_file(args.input)
+        
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
+    
+    # Setting cache
+    cached_kmer = disk_cache(cache_dir = args.output)(Sequence.expensive_kmer)
+    
+    # Creating one dict for whole file
+    all_kmers ={}
+    for seq in records:
+        kmer_dict = cached_kmer(seq)
+        all_kmers[seq.header] = kmer_dict
+    
+    # Writing out file to specyfic file
+    all_kmers_path = os.path.normpath(os.path.join(args.output, 'all_kmers.pkl'))
+    with open(all_kmers_path, 'wb') as out_f:
+        pickle.dump(all_kmers, out_f)
+        
+    print(f"Wrote {len(all_kmers)} records to {all_kmers_path} (with file combining them all named: all_kmers.pkl)")
+    
+    if args.clear_cache:
+        for file in os.listdir(args.output):
+            to_remove = re.match(".*pkl", file).string
+            os.remove(f'{args.output}/{to_remove}')
+
+    # loading files and creating old_files list
+    old_files = []
+    for file in os.listdir(args.output):
+        # defining names of files to later loop through
+        filepath = os.path.normpath(os.path.join(args.output, file))
+        
+        # calculatnig file age
+        mtime = os.path.getmtime(filepath)
+        file_age = datetime.fromtimestamp(mtime).date()
+        now = datetime.now().date()
+        age = now - file_age
+        if age.days >= 7:
+            old_files.append(filepath)
+    
+    with zipfile.ZipFile(zip_path, 'w') as archive:
+        for filename in old_files:
+            archive.write(filename, arcname=os.path.basename(filename))
+    
+    for filepath in old_files:
+        os.remove(filepath)
+    
+
+if __name__ == "__main__":
+    main()
